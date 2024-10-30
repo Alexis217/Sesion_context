a86b2c035e09bb256740a35eeae41cbcc58ed33f
Alexis Albarenga <albarengaalexis2@gmail.com>
2024-10-30 12:01:26 -0300

    Se creo distibucion de carpetas

---
+++ Sesion_Context/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ajusta según los archivos que uses
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
